7eb99c5d46bf036c7c1a1a58a1d77f2d1cca8b98
Andrew Servi <andrew@healthlabs.com>
2018-12-06 17:03:17 -0600

    Got the not out of assertQaIDIsFullyVisible and made assertQaIDIsNotFullyVisible

--- src/Medology/Behat/Mink/QualityAssurance.php
+++ src/Medology/Behat/Mink/QualityAssurance.php
@@ -1,12 +1,13 @@
 <?php namespace Medology\Behat\Mink;
 
 use Behat\Behat\Context\Context;
-use Behat\Mink\Exception\DriverException;
+use Behat\Mink\Element\NodeElement;
 use Behat\Mink\Exception\ExpectationException;
 use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Medology\Behat\UsesStoreContext;
 use Medology\SpinnerTimeoutException;
 use ReflectionException;
+use WebDriver\Exception as WebDriverException;
 
 class QualityAssurance implements Context
 {
@@ -14,38 +15,89 @@ class QualityAssurance implements Context
     use UsesStoreContext;
 
     /**
+     * Get a NodeElement by qaId
+     *
+     * @param $qaId string the qaId of the Element to get
+     *
+     * @return NodeElement
+     *
+     * @throws ExpectationException
+     * @throws SpinnerTimeoutException
+     */
+    protected function getNodeElementByQaID($qaId) {
+        $this->flexibleContext->waitForPageLoad();
+        return $this->flexibleContext->getSession()->getPage()->find('xpath', '//*[@data-qa-id="' . $qaId . '"]');
+    }
+
+    /**
+     * Get a array of NodeElements by qaId
+     *
+     * @param $qaId string the qaId of the Elements to get
+     *
+     * @return NodeElement[]
+     *
+     * @throws ExpectationException
+     * @throws SpinnerTimeoutException
+     */
+    protected function getNodeElementsByQaID($qaId) {
+        $this->flexibleContext->waitForPageLoad();
+        return $this->flexibleContext->getSession()->getPage()->findAll('xpath', '//*[@data-qa-id="' . $qaId . '"]');
+    }
+
+    /**
      * Asserts that a qaId is fully visible.
      *
-     * @Then /^"(?P<qaId>[^"]+)" should(?P<not> not|) be fully visible$/
+     * @Then /^"(?P<qaId>[^"]+)" should be fully visible$/
      *
-     * @param  string                           $qaId The qaId of the dom element to find
-     * @param  bool                             $not  Asserts qaId is partially or not visible in the viewport.
-     * @throws DriverException                  When the operation cannot be done
-     * @throws ExpectationException             If the element is not (or is) fully visible
+     * @param string $qaId
+     * @throws ExpectationException             If the element is fully visible
      * @throws ReflectionException              If injectStoredValues incorrectly believes one or more closures were
      *                                          passed. This should never happen. If it does, there is a problem with
      *                                          the injectStoredValues method.
      * @throws SpinnerTimeoutException          If the timeout expired before the assertion could be run even once.
      * @throws UnsupportedDriverActionException When operation not supported by the driver
+     * @throws WebDriverException
      */
-    public function assertQaIDIsFullyVisible($qaId, $not = false)
+    public function assertQaIDIsFullyVisible($qaId)
     {
-        $qaId = $this->storeContext->injectStoredValues($qaId);
-
         $this->flexibleContext->waitForPageLoad();
 
-        $element = $this->flexibleContext->getSession()->getPage()->find('xpath', '//*[@data-qa-id="' . $qaId . '"]');
-        if (!$element) {
-            if ($not) {
-                return;
-            }
+        $element = $this->getNodeElementByQaID($this->storeContext->injectStoredValues($qaId));
 
+        if (!$element) {
             throw new ExpectationException(
                 "Data QA ID '$qaId' is not visible, but it should be",
                 $this->flexibleContext->getSession()
             );
         }
 
-        $this->flexibleContext->assertElementIsFullyVisible($element, $not);
+        $this->flexibleContext->assertElementIsFullyVisible($element);
+    }
+
+    /**
+     * Asserts that a qaId is fully visible.
+     *
+     * @Then /^"(?P<qaId>[^"]+)" should not be fully visible$/
+     *
+     * @param string $qaId
+     * @throws ExpectationException             If the element is fully visible
+     * @throws ReflectionException              If injectStoredValues incorrectly believes one or more closures were
+     *                                          passed. This should never happen. If it does, there is a problem with
+     *                                          the injectStoredValues method.
+     * @throws SpinnerTimeoutException          If the timeout expired before the assertion could be run even once.
+     * @throws UnsupportedDriverActionException
+     * @throws WebDriverException
+     */
+    public function assertQaIDIsNotFullyVisible($qaId)
+    {
+        $this->flexibleContext->waitForPageLoad();
+
+        $element = $this->getNodeElementByQaID($this->storeContext->injectStoredValues($qaId));
+
+        if (!$element) {
+            return;
+        }
+
+        $this->flexibleContext->assertElementIsNotFullyVisible($element);
     }
 }

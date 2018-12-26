d3d2885c0b827fd854b9efc8660ba1afcf092b16
Andrew Servi <serv92@servwell.pro>
2018-12-26 10:54:23 -0600

    Code review changes

--- src/Medology/Behat/Mink/FlexibleContext.php
+++ src/Medology/Behat/Mink/FlexibleContext.php
@@ -1379,8 +1379,7 @@ class FlexibleContext extends MinkContext
      */
     public function nodeIsFullyVisibleInViewport(NodeElement $element)
     {
-        /** @var Selenium2Driver $driver */
-        $driver = $this->getSession()->getDriver();
+        $driver = $this->assertSelenium2Driver('Checks if a node Element is fully visible in the viewport.');
         if (!$driver->isDisplayed($element->getXpath()) ||
             count(($parents = $this->getListOfAllNodeElementParents($element, 'html'))) < 1
         ) {
@@ -1389,7 +1388,7 @@ class FlexibleContext extends MinkContext
         $elementViewportRectangle = $this->getElementViewportRectangle($element);
         foreach ($parents as $parent) {
             if (!$parent->isVisible() ||
-                !$elementViewportRectangle->contains($this->getElementViewportRectangle($parent))
+                !$elementViewportRectangle->isContainedIn($this->getElementViewportRectangle($parent))
             ) {
                 return false;
             }
@@ -1408,8 +1407,8 @@ class FlexibleContext extends MinkContext
      */
     public function nodeIsVisibleInViewport(NodeElement $element)
     {
-        /** @var Selenium2Driver $driver */
-        $driver = $this->getSession()->getDriver();
+        $driver = $this->assertSelenium2Driver('Checks if a node Element is visible in the viewport.');
+
         $parents = $this->getListOfAllNodeElementParents($element, 'html');
 
         if (!$driver->isDisplayed($element->getXpath()) || count($parents) < 1) {
@@ -1430,20 +1429,14 @@ class FlexibleContext extends MinkContext
     /**
      * Checks if a node Element is visible in the document.
      *
-     * @param  NodeElement $element NodeElement to to check for in the document
-     * @throws Exception   If cannot get the Web Driver
+     * @param  NodeElement $element             NodeElement to to check for in the document
+     * @throws Exception                        If cannot get the Web Driver
+     * @throws UnsupportedDriverActionException If driver is not the selenium 2 driver
      * @return bool
      */
     public function nodeIsVisibleInDocument(NodeElement $element)
     {
-        /** @var Selenium2Driver $driver */
-        $driver = $this->getSession()->getDriver();
-
-        if (!$driver->isDisplayed($element->getXpath())) {
-            return false;
-        }
-
-        return true;
+        return $this->assertSelenium2Driver('Check if element is displayed')->isDisplayed($element->getXpath());
     }
 
     /**
--- src/Medology/Behat/Mink/Models/Geometry/Rectangle.php
+++ src/Medology/Behat/Mink/Models/Geometry/Rectangle.php
@@ -39,15 +39,27 @@ class Rectangle
     /**
      * Checks if this is fully inside another rectangle.
      *
-     * @param Rectangle $rectangle Rectangle to check against this one
+     * @param  Rectangle $rectangle Rectangle to check against this one
+     * @return bool
      */
-    public function contains(self $rectangle)
+    public function isContainedIn(self $rectangle)
     {
         return
             $this->left >= $rectangle->left &&
             $this->right <= $rectangle->right &&
             $this->top >= $rectangle->top &&
             $this->bottom <= $rectangle->bottom;
+    }
+
+    /**
+     * Checks if the specified rectangle overlaps with this rectangle.
+     *
+     * @param  Rectangle $rectangle Rectangle to check against this one
+     * @return bool
+     */
+    public function overlaps(self $rectangle)
+    {
+        return $this->overlapsInY($rectangle) && $this->overlapsInX($rectangle);
     }
 
     /**
@@ -58,44 +70,29 @@ class Rectangle
      */
     private function overlapsInX(self $rectangle)
     {
-        /** @var bool $leftOverlap If overlaps on the left */
-        $leftOverlap = $this->right <= $rectangle->right && $this->right >= $rectangle->left;
-
-        /** @var bool $leftOverlap If overlaps on the right */
-        $rightOverlap = $this->left >= $rectangle->left && $this->left <= $rectangle->right;
-
-        /** @var bool $leftOverlap If overlaps on the left and right */
-        $leftAndRightOverlap = $this->left <= $rectangle->left && $this->right >= $rectangle->right;
-
-        return $leftOverlap || $rightOverlap || $leftAndRightOverlap;
+        return
+            //If overlaps on the left
+            $this->right <= $rectangle->right && $this->right >= $rectangle->left ||
+            //If overlaps on the right
+            $this->left >= $rectangle->left && $this->left <= $rectangle->right ||
+            //If overlaps on the left and right
+            $this->left <= $rectangle->left && $this->right >= $rectangle->right;
     }
 
     /**
      * Checks if the specified rectangle overlaps with this rectangle on the Y-axis.
      *
-     * @param Rectangle $rectangle Rectangle to check against this one
+     * @param  Rectangle $rectangle Rectangle to check against this one
+     * @return bool
      */
     private function overlapsInY(self $rectangle)
     {
-        /** @var bool $topOverlap If the top overlaps */
-        $topOverlap = $this->top >= $rectangle->top && $this->top <= $rectangle->bottom;
-
-        /** @var bool $isOverlappingAtBottom If the bottom overlaps */
-        $isOverlappingAtBottom = $this->bottom <= $rectangle->bottom && $this->bottom >= $rectangle->top;
-
-        /** @var bool $isOverlappingAtTopAndBottom If the top and bottom overlaps */
-        $isOverlappingAtTopAndBottom = $this->top <= $rectangle->top && $this->bottom >= $rectangle->bottom;
-
-        return $topOverlap || $isOverlappingAtBottom || $isOverlappingAtTopAndBottom;
-    }
-
-    /**
-     * Checks if the specified rectangle overlaps with this rectangle.
-     *
-     * @param Rectangle $rectangle Rectangle to check against this one
-     */
-    public function overlaps(self $rectangle)
-    {
-        return $this->overlapsInY($rectangle) && $this->overlapsInX($rectangle);
+        return
+            //If the top overlaps
+            $this->top >= $rectangle->top && $this->top <= $rectangle->bottom ||
+            //If the bottom overlaps
+            $this->bottom <= $rectangle->bottom && $this->bottom >= $rectangle->top ||
+            //If the top and bottom overlaps
+            $this->top <= $rectangle->top && $this->bottom >= $rectangle->bottom;
     }
 }

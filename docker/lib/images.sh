7ba961de67985cb856d04acdc258f9839430e3ba
Andrew Servi <andrew@healthlabs.com>
2019-03-20 17:14:24 -0500

    .1Compatibility_2.0: Code review patch.

--- docker/lib/images.sh
+++ docker/lib/images.sh
@@ -6,4 +6,4 @@ export PHP5_TAG=5.6.40.b-behat3.4
 export PHP7_TAG=7.2.16-behat3.4
 
 export PHP5_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP5_TAG}
-export PH75_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}
+export PHP7_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}

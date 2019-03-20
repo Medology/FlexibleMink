48e323f6443bc7eb10bf0a625d185f112b71f011
Andrew Servi <andrew@healthlabs.com>
2019-03-20 17:05:55 -0500

    .1Compatibility_2.0: Code review patch.

--- docker/lib/php_image.sh
+++ docker/lib/php_image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$USE_PHP5" = "1" ]; then
-    export RESOLVED_PHP_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP5_TAG}
+    export RESOLVED_PHP_IMAGE=${PHP5_IMAGE}
 else
-    export RESOLVED_PHP_IMAGE=${PHP_OWNER}/${PHP_REPO}:${PHP7_TAG}
+    export RESOLVED_PHP_IMAGE=${PHP7_IMAGE}
 fi

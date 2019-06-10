b9fd87594740ce32246279cdd6f50ea8514a01c4
joel <joel@healthlabs.com>
2019-06-10 12:02:11 -0500

    code review changes

--- tests/Medology/Behat/Mink/FlexibleContext/PressButtonTest.php
+++ tests/Medology/Behat/Mink/FlexibleContext/PressButtonTest.php
@@ -7,12 +7,19 @@ use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Behat\Mink\Session;
 use Exception;
 use Medology\Behat\Mink\FlexibleContext;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
 
 /**
  * @covers \Medology\Behat\Mink\FlexibleContext::pressButton()
  */
 class PressButtonTest extends FlexibleContextTest
 {
+    /**
+     * Create a flexible context with some methods declared for mocking that are used in pressButton.
+     *
+     * @param  array                      $additional_methods Additional methods not specified that may be needed.
+     * @return MockObject|FlexibleContext
+     */
     protected function getFlexibleMock(array $additional_methods = [])
     {
         $sessionMock = $this->createMock(Session::class);
@@ -72,77 +79,70 @@ class PressButtonTest extends FlexibleContextTest
     public function dataFlexibleContextExceptions()
     {
         return [
-            [
-                'flexible_context',
-                'scrollToButton',
-                $this->createMock(ExpectationException::class),
-            ],
-            [
-                'flexible_context',
-                'scrollToButton',
-                $this->createMock(UnsupportedDriverActionException::class),
-            ],
-            [
-                'flexible_context',
-                'assertNodeElementVisibleInViewport',
-                $this->createMock(ExpectationException::class),
-            ],
-            [
-                'flexible_context',
-                'assertNodeElementVisibleInViewport',
-                $this->createMock(UnsupportedDriverActionException::class),
-            ],
-            [
-                'flexible_context',
-                'assertNodeElementVisibleInViewport',
-                $this->createMock(Exception::class),
-            ],
-            [
-                'button',
-                'getAttribute',
-                $this->createMock(DriverException::class),
-            ],
-            [
-                'button',
-                'getAttribute',
-                $this->createMock(UnsupportedDriverActionException::class),
-            ],
-            [
-                'button',
-                'press',
-                $this->createMock(UnsupportedDriverActionException::class),
-            ],
-            [
-                'button',
-                'press',
-                $this->createMock(DriverException::class),
-            ],
+            ['scrollToButton',                     $this->createMock(ExpectationException::class)],
+            ['scrollToButton',                     $this->createMock(UnsupportedDriverActionException::class)],
+            ['assertNodeElementVisibleInViewport', $this->createMock(ExpectationException::class)],
+            ['assertNodeElementVisibleInViewport', $this->createMock(UnsupportedDriverActionException::class)],
+            ['assertNodeElementVisibleInViewport', $this->createMock(Exception::class)],
         ];
     }
 
     /**
-     * Asserts that an exception called from used methods bubbles up.
-     *
-     * @param string    $mock      Name of the mock being tested.
-     * @param string    $method    Name of method called on mock being tested.
-     * @param Exception $exception Exception that should be have bubbled up.
+     * Asserts that an exception called from FlexibleContext methods bubble up.
      *
      * @dataProvider dataFlexibleContextExceptions
+     * @param string    $method    Name of method called on mock being tested.
+     * @param Exception $exception Exception that should be have bubbled up.
      */
-    public function testExceptionsThrownFromFlexibleContextMethodShouldBubbleOut($mock, $method, Exception $exception)
+    public function testExceptionsThrownFromFlexibleContextMethodsShouldBubbleOut($method, Exception $exception)
     {
         $flexible_context = $this->getFlexibleMock();
         $button = $this->createPartialMock(NodeElement::class, ['getAttribute', 'press']);
+        $button->method('getAttribute')->willReturn('enabled');
 
-        if ($mock != 'button' || $method != 'getAttribute') {
-            $button->method('getAttribute')->willReturn('enabled');
-        }
-
-        if ($mock != 'flexible_context' || $method != 'scrollToButton') {
+        if ($method != 'scrollToButton') {
             $flexible_context->method('scrollToButton')->willReturn($button);
         }
 
-        ${$mock}->method($method)->willThrowException($exception);
+        $flexible_context->method($method)->willThrowException($exception);
+        $this->expectException(get_class($exception));
+
+        $flexible_context->pressButton('dsfaljklkj');
+    }
+
+    /**
+     * Exceptions thrown when calling specified mock, method combination.
+     *
+     * @return array
+     */
+    public function dataButtonExceptions()
+    {
+        return [
+            ['getAttribute', $this->createMock(DriverException::class)],
+            ['getAttribute', $this->createMock(UnsupportedDriverActionException::class)],
+            ['press',        $this->createMock(DriverException::class)],
+            ['press',        $this->createMock(UnsupportedDriverActionException::class)],
+        ];
+    }
+
+    /**
+     * Asserts that an exception called from Button methods bubble up.
+     *
+     * @dataProvider dataButtonExceptions
+     * @param string    $method    Name of method called on mock being tested.
+     * @param Exception $exception Exception that should be have bubbled up.
+     */
+    public function testExceptionsThrownFromButtonMethodsShouldBubbleOut($method, Exception $exception)
+    {
+        $flexible_context = $this->getFlexibleMock();
+        $button = $this->createPartialMock(NodeElement::class, ['getAttribute', 'press']);
+        $flexible_context->method('scrollToButton')->willReturn($button);
+
+        if ($method != 'getAttribute') {
+            $button->method('getAttribute')->willReturn('enabled');
+        }
+
+        $button->method($method)->willThrowException($exception);
         $this->expectException(get_class($exception));
 
         $flexible_context->pressButton('dsfaljklkj');

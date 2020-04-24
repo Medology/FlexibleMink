034a7220857d7272f9f703958cc89b6c03152371
Chekote <domhnall.tilliere@gmail.com>
2020-04-24 07:57:07 -0500

    Changes for Smijo

--- src/Medology/Behat/CsvContext.php
+++ src/Medology/Behat/CsvContext.php
@@ -70,7 +70,7 @@ class CsvContext implements Context
      * Ensures that the given variable in the store is a CSV with the given column headers.
      * The CSV must contain exactly the rows given, and no more.
      *
-     * @Then   /^the "(?P<key>[^"]+)" should be CSV data with the following headers:$/
+     * @Then   the :key should be CSV data with the following headers:
      *
      * @param string    $key   the key the CSV is stored under
      * @param TableNode $table a list of headers that the CSV must match
@@ -83,23 +83,37 @@ class CsvContext implements Context
 
         $actualHeaders = str_getcsv(str_getcsv($this->storeContext->get($key), "\n")[0]);
 
-        $this->assertCsvHeadersMatch($expectedHeaders, $actualHeaders, $key, 'is missing');
-        $this->assertCsvHeadersMatch($actualHeaders, $expectedHeaders, $key, 'contains extra');
+        $this->assertColumnsArePresent($expectedHeaders, $actualHeaders, $key);
+        $this->assertNoAdditionalColumns($expectedHeaders, $actualHeaders, $key);
     }
 
     /**
-     * Asserts that certain CSV headers are present.
+     * Asserts that required CSV headers are present.
      *
-     * @param  string[]  $required the CSV headers that are required to be present.
-     * @param  string[]  $actual   the CSV headers that are actually present.
-     * @param  string    $csvName  the name of the CSV (for the exception message).
-     * @param  string    $context  the context of what's being checked (for the exception message).
-     * @throws Exception if the required headers are not present in the actual headers.
+     * @param  string[]  $required the headers that are required to be present.
+     * @param  string[]  $actual   the headers that are actually present.
+     * @param  string    $csvName  The name of the CSV (for the exception message).
+     * @throws Exception if any of the required headers are not present.
      */
-    private function assertCsvHeadersMatch($required, $actual, $csvName, $context)
+    private function assertColumnsArePresent(array $required, array $actual, $csvName)
     {
         if ($diff = array_diff($required, $actual)) {
-            throw new Exception("CSV '$csvName' $context headers '" . implode("', '", $diff) . "'");
+            throw new Exception("CSV '$csvName' is missing headers '" . implode("', '", $diff) . "'");
+        }
+    }
+
+    /**
+     * Asserts that no additional CSV headers are present.
+     *
+     * @param  string[]  $required the headers that are required to be present.
+     * @param  string[]  $actual   the headers that are actually present.
+     * @param  string    $csvName  The name of the CSV (for the exception message).
+     * @throws Exception if any additional headers are present.
+     */
+    private function assertNoAdditionalColumns(array $required, array $actual, $csvName)
+    {
+        if ($diff = array_diff($actual, $required)) {
+            throw new Exception("CSV '$csvName' contains extra headers '" . implode("', '", $diff) . "'");
         }
     }
 }
